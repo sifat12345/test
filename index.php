ff3f7f553b1a34020336fed0eb7148671507c364
Sifat Hossain <sifatsg@gmail.com>
2025-02-03 22:51:29 +0600

    version 2

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>This is a Git Hub</h1>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>This is a Git Hub</h1>
+        <h2>This is My First Project</h2>
+        <h2>Hello.....</h2>
+    </body>
 </html>
